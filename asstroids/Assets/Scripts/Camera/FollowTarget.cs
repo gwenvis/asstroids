a8d6ab3d5b0188357b43bdbc313646942025c10d
Antonio Bottelier <Antonio3338@gmail.com>
2017-05-15 15:10:23 +0200

    Player collision, movement toegevoegd.

---
+++ asstroids/Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] private bool lerp;
+
+    void LateUpdate()
+    {
+        
+    }
+
+}
